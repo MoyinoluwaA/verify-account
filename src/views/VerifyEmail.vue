fbd022e11780cb3b2f0fb1125d31257b163140d7
Moyinoluwa Adenuga <dayoh14@gmail.com>
2021-11-22 12:27:43 +0100

    error handling for verifying email

--- src/views/VerifyEmail.vue
+++ src/views/VerifyEmail.vue
@@ -2,8 +2,11 @@
 
     <div class="text-center pt-5 mt-5">
         <p v-if='loading' class="mt-5 pt-5">Wait while your account is being verified...</p>
-        <p v-else class="fs-3 fw-bolder mt-5 pt-5">
+        <p v-if='success' class="fs-3 fw-bolder mt-5 pt-5">
             Your account has been verified. Click <router-link to='/login'>here</router-link> to log in
+        </p>
+        <p v-if='error' class="fs-3 fw-bolder mt-5 pt-5">
+            Create an account by clicking <router-link to='/'>here</router-link>
         </p>
     </div>
 
@@ -17,7 +20,9 @@ export default {
     name: 'VerifyEmail',
     data() {
         return {
-            loading: true
+            loading: true,
+            error: false,
+            success: false
         }
     },
     mounted() {
@@ -26,6 +31,13 @@ export default {
             axios.patch(`${baseUrl}/api${path}`)
             .then((response) => {
                 if (response.status === 200) {
+                    this.loading = false
+                    this.success = true
+                }
+            })
+            .catch((err) => {
+                if (err.response.status === 404 || err.response.status === 500) {
+                    this.error = true
                     this.loading = false
                 }
             })
